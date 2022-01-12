aef5a37cea094b7f4bc7d48b202257669d0c5edd
hermes926 <hermes926@gmail.com>
2022-01-12 16:06:23 +0800

    New date base api

--- Phase2/database/databaseAPI.c
+++ Phase2/database/databaseAPI.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 #include "./sqlite3.h"
 
-int callback(void *NotUsed, int argc, char **argv, char **azColName);
+int check_callback(void *NotUsed, int argc, char **argv, char **azColName);
+int getfriend_callback(void *NotUsed, int argc, char **argv, char **azColName);
+bool called = false;
 
-int main(void) {
-    
-    sqlite3 *db;
-    char *err_msg = 0;
-    
-    int rc = sqlite3_open("database.db", &db);
+char friends_list[100][100];
+char friends_json[1000];
+int friends_cnt = 0;
+
+sqlite3* db;
+
+int db_init(char *dbname){
+
+    int rc = sqlite3_open(dbname, &db);
     
     if (rc != SQLITE_OK) {
         fprintf(stderr, "Cannot open database: %s\n", 
@@ -16,32 +24,97 @@ int main(void) {
         sqlite3_close(db);   
         return 1;
     }
-    
-    char *sql = "SELECT * FROM Friendship";
-        
-    rc = sqlite3_exec(db, sql, callback, 0, &err_msg);
-    
-    if (rc != SQLITE_OK ) {
-        
-        fprintf(stderr, "Failed to select data\n");
-        fprintf(stderr, "SQL error: %s\n", err_msg);
-        sqlite3_free(err_msg);
-        sqlite3_close(db);       
-        return 1;
-    } 
-    
-    sqlite3_close(db);
-    
     return 0;
 }
 
-int callback(void *NotUsed, int argc, char **argv, char **azColName) {
-    
-    NotUsed = 0;   
-    for (int i = 0; i < argc; i++) {
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }   
-    printf("\n");
-    
+int db_adduser(char username[]){
+    char sql[100];
+    char* err_msg = 0;
+    sprintf(sql, "INSERT INTO User VALUES('%s');", username);
+    int rc = sqlite3_exec(db, sql, check_callback, 0, &err_msg);
+    if (rc != SQLITE_OK) {
+        if(rc == SQLITE_CONSTRAINT){
+            return 2; // Duplicate name
+        }else      
+            return 1; // Other error
+    } 
+    return 0; // Success
+}
+
+bool db_checkuser(char username[]){
+    char sql[100];
+    char* err_msg = 0;
+    sprintf(sql, "SELECT Name FROM User WHERE Name = '%s';", username);
+    called = false;
+    int rc = sqlite3_exec(db, sql, check_callback, 0, &err_msg);
+    if (rc != SQLITE_OK || !called) {
+        return false;
+    } 
+    return true; // Success
+}
+
+char* db_getuserfriend(char username[]){
+    char sql[100];
+    char* err_msg = 0;
+    sprintf(sql, "SELECT Friend2 FROM Friendship WHERE Friend1 = '%s';", username);
+
+    int rc = sqlite3_exec(db, sql, getfriend_callback, 0, &err_msg);
+
+    if (rc != SQLITE_OK || friends_cnt == 0) {
+        return ""; // No friend
+    }
+
+    friends_json[0] = '{';
+    for(int i = 0; i < friends_cnt; i++){
+        sprintf(friends_json + strlen(friends_json), "'%s',", friends_list[i]);
+    }
+    sprintf(friends_json + strlen(friends_json), "}");
+     
+    return friends_json; // Success
+}
+
+int db_addfriendship(char username1[], char username2[]){
+    if(!db_checkuser(username1) || !db_checkuser(username2))
+        return 2;
+    char sql[100];
+    char* err_msg = 0;
+    sprintf(sql, "INSERT INTO Friendship VALUES(NULL, '%s', '%s');", username2, username1);
+    int rc = sqlite3_exec(db, sql, check_callback, 0, &err_msg);
+    if (rc != SQLITE_OK) {
+        if(rc == SQLITE_CONSTRAINT){
+            return 2; // Duplicate name
+        }else      
+            return 1; // Other error
+    } 
+    sprintf(sql, "INSERT INTO Friendship VALUES(NULL, '%s', '%s');", username1, username2);
+    rc = sqlite3_exec(db, sql, check_callback, 0, &err_msg);
+    if (rc != SQLITE_OK) {
+        if(rc == SQLITE_CONSTRAINT){
+            return 2; // Duplicate name
+        }else      
+            return 1; // Other error
+    }
+    return 0; // Success
+}
+
+int check_callback(void *NotUsed, int argc, char **argv, char **azColName) {
+    called = true;
     return 0;
 }
+
+int getfriend_callback(void *NotUsed, int argc, char **argv, char **azColName) {
+    sprintf(friends_list[friends_cnt], "%s", argv[0]);
+    friends_cnt++;
+    printf("%s\n", friends_list[friends_cnt - 1]);
+    return 0;
+}
+
+
+// int main(void) {
+//     int rc = db_init("database.db");
+//     printf(db_checkuser("dinoo") ? "yes\n" : "NO\n");
+//     printf("%d\n", db_addfriendship("Hermes", "Dino"));
+//     printf("%d\n", db_addfriendship("Albert", "Dino"));
+//     printf("%d\n", db_addfriendship("Dino", "Hermes"));
+//     return 0;
+// }
